465ebdef654b79292a03781fc2139093a06db97e
Eric Jacobs <eric.j@collava.org>
2018-01-31 19:26:33 -0600

    Removed certainty algorithm. Implemented three-level buying strategy. Fix selling asset.

--- pyTrader.py
+++ pyTrader.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.insert(0, './app')
+#sys.path.insert(0, './app')
 
 from termcolor import colored
 
@@ -9,17 +9,24 @@ from termcolor import colored
 from binance.client import Client
 
 
-api_key = MARSHALL, ADD YOUR KEY
-api_secret = MARSHALL, ADD YOUR SECRET
+from apikeys import api_key, api_secret
+
+import math
+import time
+
 
 class Strategy:
 
     def trades(self):
         client = Client(api_key, api_secret)
-        #Change ths to change the asset you're buying witg
+        # Change this to change the asset you're buying
         asset = "BNB"
-        #Change pair you're trading
-        symbol ='BNBBTC'
+        # Change this to change the asset you're paying with
+        otherasset = "USDT"
+
+        # Calculate pair you're trading
+        symbol = asset + otherasset
+
         balance= (client.get_asset_balance(asset))
         balance = float(balance['free'])
         startBalance=(client.get_asset_balance(asset))
@@ -27,20 +34,45 @@ class Strategy:
         #lastPrice = float(Orders.get_ticker(symbol)['lastPrice'])
         currentPrice = client.get_ticker(symbol =symbol)
         currentPrice=float(currentPrice['lastPrice'])
-        #MANUALLY CHANGE LINE 31 TO WHAT YOU WANT
-        firstBuyPrice = 11074.01
+        #MANUALLY CHANGE LINE 37 TO WHAT YOU WANT
+        firstBuyPrice = 13.5
+
+        # Get the min and max quantity for trading under this symbol.
+        symbolInfo = client.get_symbol_info(symbol)
+        symbolFilters = {filt['filterType']: filt
+                             for filt in symbolInfo['filters']}
+        lotSizeFilter = symbolFilters.get('LOT_SIZE', {})
+        lotSizeMinQty = float(lotSizeFilter.get('minQty', 0.0))
+        lotSizeMaxQty = float(lotSizeFilter.get('maxQty', 0.0))
+        lotSizeStep = float(lotSizeFilter.get('stepSize', 0.0))
+
+        # This is the number of digits we should round to in order
+        # to conform to the lot's stepSize restriction.
+        lotSizeStepNumDigits = int(round(-math.log(lotSizeStep, 10)))
 
         numberOfBuys = 0
         balances = client.get_account()
-       
 
+        # How much of asset we want to work with.
+        wantedBalance = 8
+
+        buyLevel1 = firstBuyPrice * 0.995	# 0.5%
+        buyLevel2 = firstBuyPrice * 0.99	# 1.0%
+        buyLevel3 = firstBuyPrice * 0.985	# 1.5%
+
+        buyAmount1 = wantedBalance * 0.25	# How much to buy at buyLevel1 price.
+        buyAmount2 = wantedBalance * 0.35	# How much to buy at buyLevel2 price.
+        buyAmount3 = wantedBalance * 0.50	# How much to buy at buyLevel3 price.
+
+
+        # TODO: Remove Certainty processing.
         lowCertainty = firstBuyPrice * .02
         highCertainty = firstBuyPrice * .03
-        sellCertainty = firstBuyPrice * .7
+        sellLevel = firstBuyPrice * 1.002
 
         buylowCertainty= float(round((balance * 0.05)/currentPrice))
         buyhighCertainty= float(round((balance * 0.10)/currentPrice))
-        sellhighCertainty= float(round(balance * 0.95))
+        sellhighAmount= float(round(balance * 0.95))
 
         lowCounter= 0
         highCounter= 0
@@ -49,37 +81,131 @@ class Strategy:
         cycle = 0
         actions = []
 
-        
+        otherbalance= (client.get_asset_balance(otherasset))
+        otherbalance = float(otherbalance['free'])
+
+        if balance < wantedBalance:
+
+            print colored('Auto Trading with binance', "cyan")
+            print colored('Waiting to buy at', "blue"), buyLevel1, buyLevel2, buyLevel3
+            print colored('Waiting to sell at', "blue"), sellLevel
+
+            while balance < wantedBalance:
+
+                time.sleep(5)
+                currentPrice = client.get_ticker(symbol=symbol)
+                currentPrice = float(currentPrice['lastPrice'])
+
+                print 'balance=', balance, asset, '  otherbalance=', otherbalance, otherasset
+                print 'currentPrice=', currentPrice, ' buyLevels=', buyLevel1, buyLevel2, buyLevel3
+
+                canBuy = round(otherbalance / currentPrice, lotSizeStepNumDigits)
+                print 'We can now buy (up to)', canBuy, 'of', asset
+
+                if currentPrice <= buyLevel3:
+                    qty = min(canBuy, wantedBalance * 0.50)
+                    print 'Hit buyLevel3', qty
+
+                    order = client.create_order(
+                        symbol=symbol,
+                        side= Client.SIDE_BUY,
+                        type=Client.ORDER_TYPE_MARKET,
+                        quantity=qty
+                    )
+                    buyLevel3 = 0
+
+                    balance = (client.get_asset_balance(asset))
+                    balance = float(balance['free'])
+                    otherbalance = (client.get_asset_balance(otherasset))
+                    otherbalance = float(otherbalance['free'])
+                    break
+
+                elif currentPrice <= buyLevel2:
+                    qty = min(canBuy, wantedBalance * 0.35)
+                    print 'Hit buyLevel2', qty
+
+                    order = client.create_order(
+                        symbol=symbol,
+                        side= Client.SIDE_BUY,
+                        type=Client.ORDER_TYPE_MARKET,
+                        quantity=qty
+                    )
+                    buyLevel2 = 0
+
+                elif currentPrice <= buyLevel1:
+                    qty = min(canBuy, wantedBalance * 0.25)
+                    print 'Hit buyLevel1', qty
+
+                    order = client.create_order(
+                        symbol=symbol,
+                        side= Client.SIDE_BUY,
+                        type=Client.ORDER_TYPE_MARKET,
+                        quantity=min(canBuy, wantedBalance * 0.25)
+                    )
+                    buyLevel1 = 0
+
+                elif currentPrice >= sellLevel and balance > lotSizeMinQty:
+                    qty = round(balance * 0.95, lotSizeStepNumDigits)
+
+                    print colored('TEST-sellLevel exceeded', "blue"),
+                    print qty
+                    order = client.create_order(
+                        symbol=symbol,
+                        side= Client.SIDE_SELL,
+                        type=Client.ORDER_TYPE_MARKET,
+                        quantity=qty
+                    )
+                    print colored('sold at ' + str(currentPrice), "blue")
+                    #increase count
+                    sellcounter= sellcounter + 1
+
+                balance = (client.get_asset_balance(asset))
+                balance = float(balance['free'])
+                otherbalance = (client.get_asset_balance(otherasset))
+                otherbalance = float(otherbalance['free'])
 
         print colored('Auto Trading with binance', "cyan")
         print colored('Waiting to sell at', "blue")
-        print  sellCertainty
-  
+        print sellLevel
 
-        
         while balance>= (startBalance/2):
+
+            time.sleep(5)
+            currentPrice = client.get_ticker(symbol =symbol)
+            currentPrice=float(currentPrice['lastPrice'])
+
+            print 'balance=', balance
+            print 'currentPrice=', currentPrice, '  sellLevel=', sellLevel
+
             #Line 61 is not getting triggered all other statements seem fine
-            if currentPrice >= sellCertainty:
+            if currentPrice >= sellLevel:
                 #it never prints this. WHY? Code has forsaken me!
                 print colored('TEST', "blue")
                 order = client.create_order(
-                    symbol='BTCUSDT',
+                    symbol=symbol,
                     side= Client.SIDE_SELL,
                     type=Client.ORDER_TYPE_MARKET,
-                    quantity=sellhighCertainty
+                    quantity=sellhighAmount
                 )
-                print colored('sold at high certainty', "blue")
+                print colored('sold at ' + str(currentPrice), "blue")
                 #increase count
                 sellcounter= sellcounter + 1
                 #recheck balance
-                balance= (client.get_asset_balance('BTC'))
+                balance= (client.get_asset_balance(asset))
                 balance = float(balance['free'])
 
+            continue
+
+            #
+            # TODO: Convert remaining code in this block to new three-level
+            # strategy.
+            #
+
             #Checks to see if it should buy at low settings
-            elif lowCounter <= 1 and currentPrice< highCertainty:
+            if lowCounter <= 1 and currentPrice< highCertainty:
                 if currentPrice >= lowCertainty and currentPrice< highCertainty:
                     order = client.create_order(
-                    symbol='BTCUSDT',
+                    symbol=symbol,
                     side= Client.SIDE_BUY,
                     type=Client.ORDER_TYPE_MARKET,
                     quantity=buylowCertainty
@@ -87,17 +213,17 @@ class Strategy:
                     firstBuyPrice = currentPrice
                     print colored('bought at low certainty', "blue")
                     #recheck the balance
-                    balance= (client.get_asset_balance('USDT'))
+                    balance= (client.get_asset_balance(asset))
                     balance = float(balance['free'])
                     #increase count
                     lowCounter= lowCounter +1
                     print colored('Bought at', "green")
                     print  firstBuyPrice
                     print colored('Waiting to sell at', "blue")
-                    print  sellCertainty
+                    print  sellLevel
 
              #Checks to see if it should buy at high settings
-            elif currentPrice>= highCertainty and currentPrice < sellCertainty and highCounter < 1:
+            elif currentPrice>= highCertainty and currentPrice < sellLevel and highCounter < 1:
                 order = client.create_order(
                 symbol='BTCUSDT',
                 side= Client.SIDE_BUY,
@@ -109,15 +235,15 @@ class Strategy:
                 #increase counter
                 highCounter = highCounter + 1
                 #recheck balance
-                balance= (client.get_asset_balance('BTC'))
+                balance= (client.get_asset_balance(asset))
                 balance = float(balance['free'])
                 print colored('Bought at', "green")
                 print  firstBuyPrice
                 print colored('Waiting to sell at', "blue")
-                print  sellCertainty
+                print  sellLevel
 
-            #Also tried selling here. Damn thing not working. 
-            elif currentPrice >= sellCertainty:
+            #Also tried selling here. Damn thing not working.
+            elif currentPrice >= sellLevel:
                 order =client.order_limit_sell(
                    symbol='BTCUSDT',
                    quantity=20
@@ -126,8 +252,8 @@ class Strategy:
                 #increase counter
                 sellcounter= sellcounter + 1
                 #reset balance
-                balance= (client.get_asset_balance('BTC'))
+                balance= (client.get_asset_balance(asset))
                 balance = float(balance['free'])
-                
+
 strat = Strategy()
 strat.trades()
