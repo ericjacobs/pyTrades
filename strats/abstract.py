1a155faa8fd152401ec6d5752d6591abfff90f10
Eric Jacobs <eric.j@collava.org>
2018-02-14 20:23:39 -0600

    Fix calculation of notional filter. Enable trading for all asset pairs.

--- strats/abstract.py
+++ strats/abstract.py
@@ -3,6 +3,8 @@
 # Abstract definition of a trading strategy.
 #
 
+import time
+
 from pyTrades.util import task
 
 
@@ -14,8 +16,8 @@ class AbstractStrategy(object):
     # Change this to change the asset you're paying with
     otherasset = None
 
-    def start(self):
+    def start(self, delayTime=0):
         """Start this trading bot."""
-        task.TaskGreenlet(self.trades,
+        task.TaskGreenlet(self.trades, delayTime,
                           name='%s%s Bot' % (self.asset, self.otherasset)
         ).start()
